239f2d54fbd2ff80c4a1bb6be1d6be092b4d8c76
khurasak <khurasak@sheridancollege.ca>
2019-01-30 11:27:58 -0500

    Revert "third"
    This reverts commit 8116a20af479ecc8f23b2e6bc9b592e6b6ca0fb5.

--- Card/src/Card.java
+++ Card/src/Card.java
@@ -13,26 +13,6 @@ public class Card {
     private String suit;
     //Hearts,Spades,Diamonds,Clubs
 
-    private String first;
-
-    /**
-     * Get the value of first
-     *
-     * @return the value of first
-     */
-    public String getFirst() {
-        return first;
-    }
-
-    /**
-     * Set the value of first
-     *
-     * @param first new value of first
-     */
-    public void setFirst(String first) {
-        this.first = first;
-    }
-
         private int value;
         
         public Card(String givenSuit,int givenValue)
@@ -67,10 +47,8 @@ public class Card {
         else
         {
             this.value = value;
-             System.out.println("hellow");
         }
     }
-    
     
 
     
